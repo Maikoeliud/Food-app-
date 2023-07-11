d540d459654528cf595bab624803b7af95ee7889
Maikoeliud <maikoeliud@gmail.com>
2023-07-12 00:11:31 +0300

    Meals-card

---
+++ src/meals/MealItem.js
@@ -0,0 +1,12 @@
+import classes from './MealItem.module.css'
+
+ export function MealItem(props){
+    const price = `$${props.price.toFixed(2)}`;
+    return <li className={classes.meal}>
+        <div>
+            <h3>{props.name}</h3>
+            <div className={classes.description}>{props.description}</div>
+            <div className={classes.price}>{price}</div>
+        </div>
+    </li>
+}
